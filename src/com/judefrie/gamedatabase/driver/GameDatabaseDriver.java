6c593059ef279a65b7194bb2db92355b3887f7da
Jude A. Frie <jfrie743@gmail.com>
2014-08-08 22:05:29 -0700

    Added try/catch stuff

--- src/com/judefrie/gamedatabase/driver/GameDatabaseDriver.java
+++ src/com/judefrie/gamedatabase/driver/GameDatabaseDriver.java
@@ -4,7 +4,7 @@ package com.judefrie.gamedatabase.driver;
  * Created by JudeFrie on 08/08/2014.
  */
 public class GameDatabaseDriver {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         new Startup();
     }
 }
--- src/com/judefrie/gamedatabase/driver/Startup.java
+++ src/com/judefrie/gamedatabase/driver/Startup.java
@@ -1,29 +1,38 @@
 package com.judefrie.gamedatabase.driver;
+
 import java.util.Scanner;
 
 /**
  * Created by JudeFrie on 08/08/2014.
  */
-public class Startup {
-    String input;
+public class Startup extends Throwable {
+
+    int input;
     String username;
     String password;
     String passwordCheck;
     Scanner scan;
+    NumberFormatException problem;
 
     public Startup() {
         scan = new Scanner(System.in);
+        problem = new NumberFormatException("Incorrect input");
 
-        System.out.println("Would you like to:\n1 - Login \n2 - Create an account");
-        input = scan.next();
-
-        if (input == "1") {
-            System.out.println("Please enter your username and password.");
-            username = scan.next();
-            password = scan.next();
+        System.out.println("Would you like to:\n1 - Login \n2 - Create an account\nPress q to quit");
+        try {
+            input = scan.nextInt();
+        } catch (Exception e) {
+            System.out.println("Wrong");
         }
-
-        else if (input == "2") {
+        if (input == 1) {
+            System.out.println("Please enter your username and password.");
+            try {
+                username = scan.next();
+                password = scan.next();
+            } catch (Exception e) {
+                System.out.println("Wrong");
+            }
+        } else if (input == 2) {
             System.out.println("Please enter a new user name and password.");
             username = scan.next();
             password = scan.next();
@@ -31,12 +40,16 @@ public class Startup {
             passwordCheck = scan.next();
             if (password.equals(passwordCheck))
                 ;
-            else
-                System.out.println("Password does not match previous");
+            else {
+                try {
+                    if (input != 1 || input != 2)
+                        throw problem;
+                } catch (Exception e) {
+                    System.out.println("Password does not match previous");
+                }
+            }
 
-        }
-
-        else
+        } else
             System.out.println("Incorrect input");
     }
 
