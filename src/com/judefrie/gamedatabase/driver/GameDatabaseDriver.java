19f2e12ec96457d06174fa950cb28132ce0860bd
Jude A. Frie <jfrie743@gmail.com>
2014-08-08 20:26:53 -0700

    Added some functionality

--- src/com/judefrie/gamedatabase/driver/GameDatabaseDriver.java
+++ src/com/judefrie/gamedatabase/driver/GameDatabaseDriver.java
@@ -4,4 +4,7 @@ package com.judefrie.gamedatabase.driver;
  * Created by JudeFrie on 08/08/2014.
  */
 public class GameDatabaseDriver {
+    public static void main(String[] args){
+        new Startup();
+    }
 }
